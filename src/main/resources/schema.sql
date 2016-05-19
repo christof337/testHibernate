5760b6687fad04e4e9c89ae69f986a5d25a48d46
Christophe <christof337@hotmail.fr>
2016-05-19 16:16:16 +0200

    correction insertion double
    wazaaaa

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 
 CREATE TABLE AUTHOR (id INTEGER, lastName varchar(50), firstName varchar(50))
-insert into AUTHOR values(1, 'Rowling', 'JK')
+insert into AUTHOR values(100, 'Rowling', 'JK')
 
 CREATE TABLE BOOK (id INTEGER, title varchar(50), ISBN varchar(30), summary varchar(30), published date, author_id INTEGER)
 
-insert into BOOK values(1, 'Harry Potter et ta mère', '00448U573', '"Tu es un sorcier Harry"', null, 1)
+insert into BOOK values(10, 'Harry Potter et ta mère', '00448U573', '"Tu es un sorcier Harry"', null, 1)
